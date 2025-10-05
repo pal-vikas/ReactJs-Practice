9ebaddbbae9ef48391bbdb44c7ef399f54619164
pal-vikas <vikaspal964@gmail.com>
2025-10-05 17:20:02 +0530

    first Commit

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Sellers() {
+  const [name, setName] = useState("");
+
+  useEffect(()=>{
+
+    console.log("Mounting Sellers Component......");
+  },[])
+
   return (
     <div>
-                <h1>Admin Sellers Page</h1>
+                <h4>Admin Sellers Page</h4>
+                <h5>{name}</h5>
+                <input type="text" name='name' value={name} onChange={(e)=>{setName(e.target.value)}}  />
+                
     </div>
   )
 }
