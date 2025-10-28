1b36a22127d079a1676adcbec93fd842202ee3df
pal-vikas <vikaspal964@gmail.com>
2025-10-28 17:05:22 +0530

    Added Get API fetch

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -3,17 +3,22 @@ import React, { useEffect, useState } from 'react'
 function Sellers() {
   const [name, setName] = useState("");
 
+  // useEffect(()=>{
+  //   // document.title=`Your name is ${name}`;
+  //   // const heading = document.querySelector("h4");
+  //   // heading.style.color="red";
+  //   console.log("Mounting Sellers Component......");
+  //   return ()=>{
+  //           console.log("Unmounting Sellers Component......");
+  //   }
+  // },[name])
+
   useEffect(()=>{
+      fetch("https://jsonplaceholder.typicode.com/users").then((res)=>res.json())
+      .then((data)=>console.log(data))
+      .catch((error)=>console.log(error));
 
-
-    // document.title=`Your name is ${name}`;
-    // const heading = document.querySelector("h4");
-    // heading.style.color="red";
-    console.log("Mounting Sellers Component......");
-    return ()=>{
-            console.log("Unmounting Sellers Component......");
-    }
-  },[name])
+  },[])
 
   return (
     <>
