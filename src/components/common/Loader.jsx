cbf69e140def55ddbbe35705b14a0568cb028954
pal-vikas <vikaspal964@gmail.com>
2025-10-29 07:45:27 +0530

    change class to className

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -4,15 +4,15 @@ import './Loader.css'
 function Loader() {
   return (
     <div>
-            <div class="dot-spinner">
-                <div class="dot-spinner__dot"></div>
-                <div class="dot-spinner__dot"></div>
-                <div class="dot-spinner__dot"></div>
-                <div class="dot-spinner__dot"></div>
-                <div class="dot-spinner__dot"></div>
-                <div class="dot-spinner__dot"></div>
-                <div class="dot-spinner__dot"></div>
-                <div class="dot-spinner__dot"></div>
+            <div className="dot-spinner">
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
             </div>
     </div>
   )
