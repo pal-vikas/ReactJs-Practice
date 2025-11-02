371329b5d76663b8f15ad1901616611c0caec62d
pal-vikas <vikaspal964@gmail.com>
2025-11-02 20:58:19 +0530

    Added GIT API

--- src/components/Admin/Sales.jsx
+++ src/components/Admin/Sales.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Loader from '../common/Loader';
 
 function Sales() {
+
+  const[users, setUsers]=useState([]);
+  const[errors, setErrors]=useState("");
+  const[isLoading, setIsLoading]=useState(false);
+
+
+
+useEffect(()=>{
+  fetchUser();
+},[])
+
+const fetchUser = async ()=>{
+  try {
+    setIsLoading(true)
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/userssss`)
+      .then((res)=>{
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+    
+  }catch (error) {
+      setIsLoading(false)
+      setErrors(error.message);
+      setUsers(users);
+  }
+}
+
   return (
     <div>
                 <h1>Admin Sales Page</h1>
+                {isLoading && <Loader/>}
+                {errors && <em>{errors}</em>}
+               {users.map((user)=>(
+                <p key={user.id}>{user.name}</p>
+               ))}
+
     </div>
   )
 }
